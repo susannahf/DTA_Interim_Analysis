ea7e01d50b766749c33b62466680b5676114435a
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-05-12 11:11:38 +0100

    implemented kim/demets spending functions
    with unit tests

--- DTASpendingFunctionsUnitTests.R
+++ DTASpendingFunctionsUnitTests.R
@@ -38,12 +38,41 @@ print("Testing SpendingFunctions.R")
 # Test 1: will it even source?
 test1 <- try(source("SpendingFunctions.R"))
 if(!inherits(test1, "try-error")) {
-  print("Test1 passed: can source SpendingFunctions.R")
-}
+  print("Test 1: can source SpendingFunctions.R")
+} else { print("Test 1 failed.")}
 
 # reasonable test values
-nmax=smax=9999
-p0=0.003
-p1=0.006 # we may not need this
-power=0.8 # we may not need this
 alpha=0.05
+t <- seq(0,1,0.01)
+
+# Test 2: Kim and DeMets alpha_u
+test2 <- try(KimDeMets_alpha_u(alpha, t))
+if(!inherits(test1, "try-error")) {
+  print("Test 2: can calculate Kim and DeMets alpha_u")
+  plot(t,test2,'l',main="Test 2: Kim/Demets alpha_u")
+} else { print("Test 2 failed.")}
+
+if(test2[t==0] == 0) {
+  print("Test 2: alpha_u(0)=0")
+} else  print("Test 2 failed.")
+
+if(test2[t==1] == alpha) {
+  print("Test 2: alpha_u(1)=alpha")
+} else  print("Test 2 failed.")
+
+# Test 3: Kim and DeMets alpha_l
+test3 <- try(KimDeMets_alpha_l(alpha, t))
+if(!inherits(test1, "try-error")) {
+  print("Test 3: can calculate Kim and DeMets alpha_l")
+  plot(t,test3,'l',main="Test 3: Kim/Demets alpha_l")
+} else { print("Test 3 failed.")}
+
+if(test3[t==0] == 0) {
+  print("Test 3: alpha_l(0)=0")
+} else  print("Test 3 failed.")
+
+if(test3[t==1] == (1-alpha)) {
+  print("Test 3: alpha_l(1)= 1-alpha")
+} else  print("Test 3 failed.")
+
+
--- SpendingFunctions.R
+++ SpendingFunctions.R
@@ -4,3 +4,22 @@
 # methods from Stallard/Todd 2000 
 # all mistakes are my own
 
+# simple kim and Demets approach with rho=1
+
+# this implements equation 5 in Stallard/Todd 2000
+KimDeMets_alpha_u <- function(alpha, t){
+  # check that alpha and t are both between 0 and 1
+  if(any(alpha<0 | alpha>1 | t<0 | t>1)) {
+    error("alpha and t must both be between 0 and 1")
+  }
+  return(alpha*t)
+}
+
+# this implements equation 6 in Stallard/Todd 2000
+KimDeMets_alpha_l <- function(alpha, t){
+  # check that alpha and t are both between 0 and 1
+  if(any(alpha<0 | alpha>1 | t<0 | t>1)) {
+    error("alpha and t must both be between 0 and 1")
+  }
+  return((1-alpha)*t)
+}
