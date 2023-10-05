54a8dbf9a593f9fd6f3775479245e3b6e1c52944
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-10-05 11:55:04 +0100

    added example code

---
+++ DTAexampleCode.R
@@ -0,0 +1,21 @@
+# example code for how to run DTA interim analysis
+
+source("generateDTAdata.R") # for continuousSeSp()
+source("DTAinterimAnalysis.R") # for DTAdiscreteInterimAnalysis
+
+
+# create a random data set to practice on
+testdata <- data.frame( 
+  reference = as.logical(rbinom(100,1,0.2)),
+  index = as.logical(rbinom(100,1,0.4))
+)
+
+# continuousSeSp adds TP and TN columns, which are needed for interim analysis code
+testwithTPTN <- continuousSeSp(testdata)
+
+# run interim analyses at n=20, 50, and 100
+# and cutoffs of 0.6 for both Se and Sp
+# the minimal set of columns for this code are "reference", "TP", and "TN"
+test1 <- DTAdiscreteInterimAnalysis(testwithTPTN,c(20,50,100), pSe=0.6, pSp=0.6)
+
+
