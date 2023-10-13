b392ea9bbc56b0023ff224634102972f76957f5d
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-10-13 17:09:58 +0100

    further replication

--- DTAFlemingDebug.R
+++ DTAFlemingDebug.R
@@ -107,3 +107,70 @@ print("Test data 2, n=100:")
 print(test2short)
 
 # this gives a few situations where it would appear actual stopping and predicted stopping do not match
+
+#next plan
+# 2) check the stopping point code
+# 3) replicate the stopping using only the original Fleming code
+
+# 1) visualise the plots around the stopping points
+#Se 200
+plot(x,contshort1$Se,'l',col="red",ylim=c(0,1), main="Continuous Se, data set 1, n=200, p0=0.6", ylab="Sensitivity",
+     xlim=c(test1$Sensitivity$terminateAt-10, test1$Sensitivity$terminateAt+10))
+lines(x, sep0_60$invH0limit, col="red", 'l', lty="dashed")
+lines(x, sep0_60$invH1limit, col="red", 'l', lty="dotdash")
+lines(x, rep(0.6,200), col="red")
+grid(nx=2)
+#Se 100
+plot(xs,contshorter1$Se,'l',col="red",ylim=c(0,1), main="Continuous Se, data set 1, n=100, p0=0.6", ylab="Sensitivity",
+     xlim=c(test1short$Sensitivity$terminateAt-10, test1short$Sensitivity$terminateAt+10))
+lines(xs, sep0_60short$invH0limit, col="red", 'l', lty="dashed")
+lines(xs, sep0_60short$invH1limit, col="red", 'l', lty="dotdash")
+lines(xs, rep(0.6,100), col="red")
+grid(nx=2)
+#Sp 200
+plot(x,contshort1$Sp,'l',col="blue",ylim=c(0,1), main="Continuous Sp, data set 1, n=200, p0=0.9", ylab="Specificity",
+     xlim=c(test1$Specificity$terminateAt-10, test1$Specificity$terminateAt+10))
+lines(x, spp0_90$invH0limit, col="blue", 'l', lty="dashed")
+lines(x, spp0_90$invH1limit, col="blue", 'l', lty="dotdash")
+lines(x, rep(0.9,200), col="blue")
+grid(nx=2)
+#Sp 100
+plot(xs,contshorter1$Sp,'l',col="blue",ylim=c(0,1), main="Continuous Sp, data set 1, n=100, p0=0.9", ylab="Specificity",
+     xlim=c(test1short$Specificity$terminateAt-10, test1short$Specificity$terminateAt+10))
+lines(xs, spp0_90short$invH0limit, col="blue", 'l', lty="dashed")
+lines(xs, spp0_90short$invH1limit, col="blue", 'l', lty="dotdash")
+lines(xs, rep(0.9,100), col="blue")
+grid(nx=2)
+# plot continuous Se and Sp for contshort2
+#Se 200
+plot(x,contshort2$Se,'l',col="red",ylim=c(0,1), main="Continuous Se, data set 2, n=200, p0=0.6", ylab="Sensitivity",
+     xlim=c(test2$Sensitivity$terminateAt-10, test2$Sensitivity$terminateAt+10))
+lines(x, sep0_60$invH0limit, col="red", 'l', lty="dashed")
+lines(x, sep0_60$invH1limit, col="red", 'l', lty="dotdash")
+lines(x, rep(0.6,200), col="red")
+grid(nx=2)
+#Se 100
+plot(xs,contshorter2$Se,'l',col="red",ylim=c(0,1), main="Continuous Se, data set 2, n=100, p0=0.6", ylab="Sensitivity",
+     xlim=c(test2short$Sensitivity$terminateAt-10, test2short$Sensitivity$terminateAt+10))
+lines(xs, sep0_60short$invH0limit, col="red", 'l', lty="dashed")
+lines(xs, sep0_60short$invH1limit, col="red", 'l', lty="dotdash")
+lines(xs, rep(0.6,100), col="red")
+grid(nx=2)
+#Sp 200
+plot(x,contshort2$Sp,'l',col="blue",ylim=c(0,1), main="Continuous Sp, data set 2, n=200, p0=0.9", ylab="Specificity",
+     xlim=c(test2$Specificity$terminateAt-10, test2$Specificity$terminateAt+10))
+lines(x, spp0_90$invH0limit, col="blue", 'l', lty="dashed")
+lines(x, spp0_90$invH1limit, col="blue", 'l', lty="dotdash")
+lines(x, rep(0.9,200), col="blue")
+grid(nx=2)
+#Sp 100
+plot(xs,contshorter2$Sp,'l',col="blue",ylim=c(0,1), main="Continuous Sp, data set 2, n=100, p0=0.9", ylab="Specificity",
+     xlim=c(test2short$Specificity$terminateAt-10, test2short$Specificity$terminateAt+10))
+lines(xs, spp0_90short$invH0limit, col="blue", 'l', lty="dashed")
+lines(xs, spp0_90short$invH1limit, col="blue", 'l', lty="dotdash")
+lines(xs, rep(0.9,100), col="blue")
+grid(nx=2)
+
+# stopping points do not correspond with the theory. They are close for the n=200, but not **identical**
+
+
