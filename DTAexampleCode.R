86972fd6e0bbb96264d8519d1b32b1648bc05432
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-11-20 13:21:25 +0000

    added clarifications to example code

--- DTAexampleCode.R
+++ DTAexampleCode.R
@@ -14,22 +14,27 @@ testdata <- data.frame(
 testwithTPTN <- continuousSeSp(testdata)
 
 # run interim analyses at n=20, 50, and 100
+# assuming that the planned sample size is 150
 # and cutoffs of 0.6 for both Se and Sp
 # the minimal set of columns for this code are "reference", "TP", and "TN"
-test1 <- DTAdiscreteInterimAnalysis(testwithTPTN,c(20,50,100), pSe=0.6, pSp=0.6, N=100, prevalence = 0.2)
+print("Interim analysis at 20, 50, and 100 points")
+test1 <- DTAdiscreteInterimAnalysis(testwithTPTN,c(20,50,100), pSe=0.6, pSp=0.6, N=150, prevalence = 0.2)
 print(test1)
 
-test2 <- DTAdiscreteInterimAnalysis(testwithTPTN,1:100, pSe=0.6, pSp=0.6, N=100, prevalence = 0.2)
+print("Continuous interim analysis up to 100 points")
+test2 <- DTAdiscreteInterimAnalysis(testwithTPTN,1:100, pSe=0.6, pSp=0.6, N=150, prevalence = 0.2)
 print(test2)
 
 # you can also run interim analyses using just the cumulative counts
 # this is the data from dataset2
+print("Interim analysis using cumulative counts")
 cumulativedata <- data.frame(N=c(20,50,100),
                              RefT=c(13,29,51),
                              TP=c(9, 22, 35),
                              TN=c(6, 19, 51))
 test3 <- DTAcumulativeInterimAnalysis(cumulativedata, pSe=0.8, pSp=0.9, N=200, prevalence = 0.4)
 print(test3)
+
 
 
 
