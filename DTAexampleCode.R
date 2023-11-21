23577ae2e89c3b1e03f5cb5362079452fe205abe
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-11-21 14:21:29 +0000

    removed example continuous method

--- DTAexampleCode.R
+++ DTAexampleCode.R
@@ -21,10 +21,6 @@ print("Interim analysis at 20, 50, and 100 points")
 test1 <- DTAdiscreteInterimAnalysis(testwithTPTN,c(20,50,100), pSe=0.6, pSp=0.6, N=150, prevalence = 0.2)
 print(test1)
 
-print("Continuous interim analysis up to 100 points")
-test2 <- DTAdiscreteInterimAnalysis(testwithTPTN,1:100, pSe=0.6, pSp=0.6, N=150, prevalence = 0.2)
-print(test2)
-
 # you can also run interim analyses using just the cumulative counts
 # this is the data from dataset2
 print("Interim analysis using cumulative counts")
@@ -32,8 +28,8 @@ cumulativedata <- data.frame(N=c(20,50,100),
                              RefT=c(13,29,51),
                              TP=c(9, 22, 35),
                              TN=c(6, 19, 51))
-test3 <- DTAcumulativeInterimAnalysis(cumulativedata, pSe=0.8, pSp=0.9, N=200, prevalence = 0.4)
-print(test3)
+test2 <- DTAcumulativeInterimAnalysis(cumulativedata, pSe=0.8, pSp=0.9, N=200, prevalence = 0.4)
+print(test2)
 
 
 
