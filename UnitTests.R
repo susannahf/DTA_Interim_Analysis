270a3c76ac722ed84902645aae354fbfe012026b
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-11-21 15:41:37 +0000

    adjusted 2nd test for max k=5

--- UnitTests.R
+++ UnitTests.R
@@ -20,7 +20,7 @@ for(i in seq(10)) {
   k=5
   p0 <- 0.4
   # k random interim analysis points
-  anaPoints <- c(sort(round(runif(k-1, 10, totaln-10))), totaln)
+  anaPoints <- c(sort(sample(10:(totaln-10), k-1)), totaln)
   # cumulative
   cdata <- cumsum(rdata)[anaPoints]
   # non-cumulative
@@ -43,29 +43,48 @@ print("Test passed: discreteInterimFleming and cumulDiscreteInterimFleming agree
 
 # run 10 times
 for(i in seq(10)) {
-  # create random data for Se and Sp with p of 0.8 and 0.6
-  p0Se <- 0.8
-  p0Sp <- 0.6
-  # prevalence = 0.2, N=100, so 20 Se data points and 80 Sp data points
-  prevalence <- 0.2
-  N <- 100
-  NSe <- prevalence*N
-  NSp <- N-NSe
-  TP <- rbinom(NSe,1,p0Se)
-  TN <- rbinom(NSp,1, p0Sp)
+  p0Se <- 0.65
+  p0Sp <- 0.85
+  prevalence <- 0.35
+  N <- 200
+
+  # create data
+  tbt <- create2x2(n=10*N, p=prevalence, se=p0Se, sp=p0Sp, digits=4, verbose=FALSE)
+  dtadata <- DTAdataFrom2x2(tbt, randomise = T)
+  dtadata <- continuousSeSp(dtadata[1:N, ])
+  
+  # analysis points
+  k <- 5
+  anaPoints <- sort(sample(10:150, k))
+  
   # pull out data for cumulDiscreteInterimFleming and run it
-  SeNs <- c(seq(NSe), rep(NSe,NSp))
-  SpNs <- c(rep(0,NSe), seq(NSp))
-  SeEvents <- c(cumsum(1-TP), rep(sum(1-TP),NSp))
-  SpEvents <- c(rep(0,NSe), cumsum(1-TN))
-  SeFleming <- cumulDiscreteInterimFleming(ns=SeNs, events=SeEvents, finaln=NSe, p0=1-p0Se, alpha=0.05)
-  SpFleming <- cumulDiscreteInterimFleming(ns=SpNs, events=SpEvents, finaln=NSp, p0=1-p0Sp, alpha=0.05)
+  cumframe <- data.frame(N=numeric(),
+                         RefT=numeric(), 
+                         TP=numeric(),
+                         TN=numeric())
+  for(i in seq(length(anaPoints))) {
+    k <- anaPoints[i]
+    ifrm <- data.frame(N = k, 
+                       RefT = sum(dtadata$reference[1:k]), 
+                       TP = sum(dtadata$TP[1:k]),
+                       TN = sum(dtadata$TN[1:k]))
+    cumframe <- rbind(cumframe,ifrm)
+  }
+  nsSe <- cumframe$RefT
+  nsSp <- cumframe$N - cumframe$RefT
+  ekSe <- nsSe-cumframe$TP
+  ekSp <- nsSp-cumframe$TN
+  NSe <- sum(dtadata$reference)
+  NSp <- N-NSe
+  
+  
+  SeFleming <- cumulDiscreteInterimFleming(ns=nsSe, events=ekSe, finaln=NSe, p0=1-p0Se, alpha=0.05)
+  SpFleming <- cumulDiscreteInterimFleming(ns=nsSp, events=ekSp, finaln=NSp, p0=1-p0Sp, alpha=0.05)
   
   # pull out data for DTAdiscreteInterimAnalysis and run it
-  dtadata <- data.frame(reference=c(rep(TRUE, NSe), rep(FALSE, NSp)),
-                        index=c(as.logical(TP), !as.logical(TN)))
-  dtadata <- continuousSeSp(dtadata)
-  dtares <- DTAdiscreteInterimAnalysis(dtadata,seq(N), pSe=p0Se, pSp=p0Sp, prevalence = prevalence, N=N, simpleOutput = FALSE)
+  actprev <- NSe/N
+  
+  dtares <- DTAdiscreteInterimAnalysis(dtadata,analysispoints = anaPoints, pSe=p0Se, pSp=p0Sp, prevalence = actprev, N=N, simpleOutput = FALSE)
   # compare results
   SeFlemingTest <- SeFleming$details
   SpFlemingTest <- SpFleming$details
