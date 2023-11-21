6aa741b6289e63eba725df34451ccc798132c8dd
Susannah Fleming <susannah.fleming@phc.ox.ac.uk>
2023-11-21 15:50:41 +0000

    fixed unit test 3 for max k = 5

--- UnitTests.R
+++ UnitTests.R
@@ -109,28 +109,29 @@ N <- 100
 prev <- 0.4
 contshort <- continuousSeSp(testdata[1:N, ])
 
+
 #suppress warnings because of final prevalence
 suppressWarnings({
 # full outputs
 # early termination for high Se at k=37 and Sp at k=30
-lowterm <- DTAdiscreteInterimAnalysis(contshort,seq(N), pSe=0.55, pSp=0.7, prevalence = prev, N=N, simpleOutput = FALSE)
+lowterm <- DTAdiscreteInterimAnalysis(contshort,c(30, 40, 50), pSe=0.55, pSp=0.7, prevalence = prev, N=N, simpleOutput = FALSE)
 # early termination for low Se at k=63  and Sp at k=32
-highterm <- DTAdiscreteInterimAnalysis(contshort,seq(N), pSe=0.8, pSp=0.95, prevalence = prev, N=N, simpleOutput = FALSE)
+highterm <- DTAdiscreteInterimAnalysis(contshort,c(30, 50, 70), pSe=0.8, pSp=0.95, prevalence = prev, N=N, simpleOutput = FALSE)
 # no termination by end point for either
-noterm <- DTAdiscreteInterimAnalysis(contshort,seq(25), pSe=0.85, pSp=0.9, prevalence = prev, N=25, simpleOutput = FALSE)
+noterm <- DTAdiscreteInterimAnalysis(contshort,c(15, 25), pSe=0.85, pSp=0.9, prevalence = prev, N=25, simpleOutput = FALSE)
 # pure interim, no termination for either
-interim <- DTAdiscreteInterimAnalysis(contshort,seq(25), pSe=0.85, pSp=0.9, prevalence = prev, N=N, simpleOutput = FALSE)
+interim <- DTAdiscreteInterimAnalysis(contshort,c(15, 25), pSe=0.85, pSp=0.9, prevalence = prev, N=N, simpleOutput = FALSE)
 
 
 # simple outputs
 # early termination for low Se at k=37 and Sp at k=30 
-lowterms <- DTAdiscreteInterimAnalysis(contshort,seq(N), pSe=0.55, pSp=0.7, prevalence = prev, N=N, simpleOutput = TRUE)
+lowterms <- DTAdiscreteInterimAnalysis(contshort,c(30, 40, 50), pSe=0.55, pSp=0.7, prevalence = prev, N=N, simpleOutput = TRUE)
 # early termination for high Se at k=63  and Sp at k=32
-highterms <- DTAdiscreteInterimAnalysis(contshort,seq(N), pSe=0.8, pSp=0.95, prevalence = prev, N=N, simpleOutput = TRUE)
+highterms <- DTAdiscreteInterimAnalysis(contshort,c(30, 50, 70), pSe=0.8, pSp=0.95, prevalence = prev, N=N, simpleOutput = TRUE)
 # no termination by end point for either
-noterms <- DTAdiscreteInterimAnalysis(contshort,seq(25), pSe=0.85, pSp=0.9, prevalence = prev, N=25, simpleOutput = TRUE)
+noterms <- DTAdiscreteInterimAnalysis(contshort,c(15, 25), pSe=0.85, pSp=0.9, prevalence = prev, N=25, simpleOutput = TRUE)
 # pure interim, no termination for either
-interims <- DTAdiscreteInterimAnalysis(contshort,seq(25), pSe=0.85, pSp=0.9, prevalence = prev, N=N, simpleOutput = TRUE)
+interims <- DTAdiscreteInterimAnalysis(contshort,c(15, 25), pSe=0.85, pSp=0.9, prevalence = prev, N=N, simpleOutput = TRUE)
 })
 
 # low termination
@@ -149,8 +150,8 @@ if(length(grep(lowterm$Sensitivity$terminationStage, lowterms$Sensitivity$termst
 if(length(grep(lowterm$Specificity$terminationStage, lowterms$Specificity$termstring))==0)
   stop("Test failed: string has incorrect number")
 # check string contains "efficacy"
-if(length(grep("efficacy", lowterms$Sensitivity$termstring))==0) stop("Test failed: string conclusion is incorrect")
-if(length(grep("efficacy", lowterms$Specificity$termstring))==0) stop("Test failed: string conclusion is incorrect")
+if(length(grep("efficacy", lowterms$Sensitivity$termstring))==0) stop("Test failed: string conclusion is incorrect for efficacy")
+if(length(grep("efficacy", lowterms$Specificity$termstring))==0) stop("Test failed: string conclusion is incorrect for efficacy")
 
 # high termination
 #print("Testing termination for high Se/Sp")
@@ -168,8 +169,8 @@ if(length(grep(highterm$Sensitivity$terminationStage, highterms$Sensitivity$term
 if(length(grep(highterm$Specificity$terminationStage, highterms$Specificity$termstring))==0)
   stop("Test failed: string has incorrect number")
 # check string contains "futility"
-if(length(grep("futility", highterms$Sensitivity$termstring))==0) stop("Test failed: string conclusion is incorrect")
-if(length(grep("futility", highterms$Specificity$termstring))==0) stop("Test failed: string conclusion is incorrect")
+if(length(grep("futility", highterms$Sensitivity$termstring))==0) stop("Test failed: string conclusion is incorrect for futility")
+if(length(grep("futility", highterms$Specificity$termstring))==0) stop("Test failed: string conclusion is incorrect for futillity")
 
 # no termination
 #print("Testing non termination")
@@ -180,11 +181,11 @@ if(!is.na(noterms$Specificity$terminateAt))  stop("Test failed: termination in s
 if(!is.na(noterms$Sensitivity$futility)) stop("Test failed: wrong futility in simple output")
 if(!is.na(noterms$Specificity$futility)) stop("Test failed: wrong futility in simple output")
 # check string does not contain futility or efficacy
-if(length(grep("efficacy|futility", noterms$Sensitivity$termstring))>0) stop("Test failed: string conclusion is incorrect")
-if(length(grep("efficacy|futility", noterms$Specificity$termstring))>0) stop("Test failed: string conclusion is incorrect")
+if(length(grep("efficacy|futility", noterms$Sensitivity$termstring))>0) stop("Test failed: string conclusion is incorrect for not having futility or efficacy")
+if(length(grep("efficacy|futility", noterms$Specificity$termstring))>0) stop("Test failed: string conclusion is incorrect for not having futility or efficacy")
 # check string contains end
-if(length(grep("end", noterms$Sensitivity$termstring))==0) stop("Test failed: string conclusion is incorrect")
-if(length(grep("end", noterms$Specificity$termstring))==0) stop("Test failed: string conclusion is incorrect")
+if(length(grep("final", noterms$Sensitivity$termstring))==0) stop("Test failed: string conclusion is incorrect at end")
+if(length(grep("final", noterms$Specificity$termstring))==0) stop("Test failed: string conclusion is incorrect at end")
 
 # pure interim
 #print("Testing pure interim analysis")
